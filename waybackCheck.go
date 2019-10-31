82139522bd6f7b903d43db312fe77aa9db18a67e
Emir (0xF61) <m1r4@yandex.com>
2019-10-31 21:27:48 +0300

    ADDED: Concurrency

--- waybackCheck.go
+++ waybackCheck.go
@@ -7,14 +7,20 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 )
 
 func main() {
-	//concurPtr := flag.Int("c", 4, "Concurrency")
+	concurPtr := flag.Int("c", 4, "Concurrency")
 	inPtr := flag.String("i", "waybackurls.txt", "WaybackUrl List")
 	outPtr := flag.String("o", "waybackurls.out", "Output Filename")
+	timeoutPtr := flag.Int("t", 2, "Timeout in second")
+
 	//screenShotPtr := flag.Bool("s", false, "Screenshot")
 	flag.Parse()
+	maxThreads := *concurPtr
+	var wg sync.WaitGroup
 
 	fileIn, err := os.Open(*inPtr)
 	if err != nil {
@@ -32,13 +38,28 @@ func main() {
 	scannerOut := bufio.NewWriter(fileOut)
 
 	for scannerIn.Scan() {
-		StatusCode, url, ContentLength := testGet(scannerIn.Text())
-		fmt.Println(StatusCode, ContentLength, url)
-		writeFile(StatusCode, url, ContentLength, *scannerOut)
+		wg.Add(1)
+		go worker(scannerIn.Text(), *timeoutPtr, *scannerOut, &wg)
+		for i := 0; i < maxThreads-1; i++ {
+			if scannerIn.Scan() {
+				wg.Add(1)
+				go worker(scannerIn.Text(), *timeoutPtr, *scannerOut, &wg)
+			} else {
+				break
+			}
+		}
+		wg.Wait()
 	}
 }
 
-func writeFile(StatusCode int, url string, ContentLength int64, fileOut bufio.Writer) {
+func worker(url string, timeout int, out bufio.Writer, wg *sync.WaitGroup) {
+	StatusCode, url, ContentLength := testGet(url, timeout)
+	fmt.Println(StatusCode, ContentLength, url)
+	writeFile(StatusCode, url, ContentLength, out, wg)
+}
+
+func writeFile(StatusCode int, url string, ContentLength int64, fileOut bufio.Writer, wg *sync.WaitGroup) {
+	defer wg.Done()
 	line := strconv.Itoa(StatusCode)
 	line += " " + url + " "
 	line += strconv.FormatInt(ContentLength, 10)
@@ -46,8 +67,11 @@ func writeFile(StatusCode int, url string, ContentLength int64, fileOut bufio.Wr
 	fileOut.Flush()
 }
 
-func testGet(url string) (int, string, int64) {
-	response, err := http.Get(url)
+func testGet(url string, timeout int) (int, string, int64) {
+	var netClient = &http.Client{
+		Timeout: time.Second * time.Duration(timeout),
+	}
+	response, err := netClient.Get(url)
 	if err != nil {
 		return 0, "CouldNotReq", 0
 	}
